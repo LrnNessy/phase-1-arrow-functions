cc97b783e7f12cc13c5eb3d018fd1556d78510bd
Lauren Shaughnessy <lrn.nessy@gmail.com>
2023-01-02 17:28:25 -0500

    commit tree 4a71315c4bc7ce85976d0ee5e916bd45dade9ce7

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide (){
+    console.log(2000/100);
+      return 20;
+  }
+  divide();
+
+  const square = number => number * number;
+
+  const add = (number1, number2) => {
+    console.log(number1 + number2);
+    return number1 + number2;
+  };
+  add(3,4);
+
